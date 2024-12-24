e15f3da1db126d1acb7bdb2701134914a432f265
Aryan Dwivedi <aryan.735921@gmail.comm>
2024-12-24 20:44:24 +0530

    chsarp starter

--- obj/Debug/net8.0/starterProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/starterProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("starterProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a4362d96bf5c62fcd9cf5abcf1d8a530c6dc1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("starterProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("starterProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
